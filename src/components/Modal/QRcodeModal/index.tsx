296229321f63026ef6a52682f53902db543153e6
alex.zhou.hi.man <alex.zhou.hi.man@gmail.com>
2021-11-15 20:19:09 +0800

    style: fix lint

--- src/components/Modal/QRcodeModal/index.tsx
+++ src/components/Modal/QRcodeModal/index.tsx
@@ -1,23 +1,21 @@
 import type { FC } from 'react'
 import { Modal } from 'antd'
 import QRcode from 'qrcode.react'
-import { useIntl } from 'umi'
 
 export interface qrcodeModalProps {
   visible: boolean
   url: string
-  title? : React.ReactNode
-  footer? : React.ReactNode
-  onCancel?: ()=>void
+  title?: React.ReactNode
+  footer?: React.ReactNode
+  onCancel?: () => void
 }
 
 const QRcodeModal: FC<qrcodeModalProps> = (props) => {
-  const { visible, url, footer, title, onCancel} = props
-  const intl = useIntl()
+  const { visible, url, footer, title, onCancel } = props
   return (
     <>
       <Modal
-        title= {title ?? null}
+        title={title ?? null}
         centered={true}
         visible={visible}
         closable={false}
@@ -26,7 +24,7 @@ const QRcodeModal: FC<qrcodeModalProps> = (props) => {
         footer={footer ?? null}
         onCancel={onCancel}
       >
-        <QRcode value={url} size={250}  includeMargin={false} renderAs="svg" />,
+        <QRcode value={url} size={250} includeMargin={false} renderAs="svg" />,
       </Modal>
     </>
   )
