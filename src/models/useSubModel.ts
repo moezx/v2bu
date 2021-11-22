7ddb80c358610168700417d70324df0d92b3cd73
alex.zhou.hi.man <alex.zhou.hi.man@gmail.com>
2021-11-22 22:11:17 +0800

    fix: 修复quantumult订阅链接无效的问题

--- src/models/useSubModel.ts
+++ src/models/useSubModel.ts
@@ -45,8 +45,11 @@ export default (): subModel => {
     const surgeUrl: string = `surge:///install-config?url=${subscribeUrl}&name=${title}`
     const shaowRocketContent: string = Buffer.from(subscribeUrl).toString('base64')
     const shadowrocketUrl: string = `shadowrocket://add/sub://${shaowRocketContent}?remark=${title}`
-    const quantumultXContent = encodeURIComponent(`{server_remote:[subscribeUrl,"tag=${title}"]}`)
-    const quantumultXUrl: string = `quantumult-x:///update-configuration?remote-resource=${quantumultXContent}`
+    const quantumultXContent = {"server_remote":[`${subscribeUrl},tag=${title}`]}
+    const quantumultXContentJSON = JSON.stringify(quantumultXContent)
+    const quantumultXUrl: string = `quantumult-x:///update-configuration?remote-resource=${quantumultXContentJSON.valueOf()}`
+    console.log(quantumultXUrl)
+
     return {
       clashUrl,
       surgeUrl,
